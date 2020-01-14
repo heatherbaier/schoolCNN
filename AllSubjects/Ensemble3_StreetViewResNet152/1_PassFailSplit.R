ac35d78c9dad01578d6018571a59ef56e2af1722
hmbaier <hmbaier@email.wm.edu>
2020-01-14 02:34:20 +0000

    street view

---
+++ AllSubjects/Ensemble3_StreetViewResNet152/1_PassFailSplit.R
@@ -0,0 +1,38 @@
+library(raster)
+library(rgdal)
+library(foreach)
+library(doParallel)
+library(doMC)
+registerDoMC(detectCores()-1)
+
+
+files <- list.files("./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/imagery/", full.names = TRUE)	
+length(files)
+
+y1314 <- read.csv("./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/y1314_AllSubjects.csv")
+
+table(y1314$intervention)
+
+for (i in 1:12588) {
+  
+  id <- base::substr(files[i], 65, 70)
+  school <- subset(y1314, school_id == as.numeric(id))
+  int <- school$intervention
+  
+#  print(id)
+#  print(school)
+  
+  if (int == 0) {
+      file.copy(files[i], "./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/pass/")
+  } else if (int == 1) {
+      file.copy(files[i], "./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/fail/")
+  }
+  
+}
+
+
+pass_files <- list.files("./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/pass/", full.names = TRUE)
+fail_files <- list.files("./clean/AllSubjects/Ensemble3_StreetViewResNet152/data/fail/", full.names = TRUE)
+
+length(pass_files)
+length(fail_files)
